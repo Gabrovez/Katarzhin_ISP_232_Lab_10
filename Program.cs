693caf2b663f518d186a1b4d8a6d46fc259b944c
Gabrovez <gabrovez321@gmail.com>
2025-10-28 23:16:22 +0300

    Шаг 1. Анонимные типы

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var monster = new
+            {
+                Name = "Dragon",
+                Damage= 50,
+                IsBoss = true,
+                Description = "Ancinet fire-breathong serpent"
+
+            };
+
+            Console.WriteLine(monster.Description);
+            var products = new[]
+            {
+                new {Name = "Milk", Price = 59.99, Category = "Products"},
+                new {Name = "Laptop", Price = 49990.00, Category = "Electronics"},
+                new {Name = "Teethbrush", Price = 120.50, Category = "Hygiene"}
+            };
+
+            Console.WriteLine("List of products");
+            foreach(var product in products)
+            {
+                Console.WriteLine($"{product.Name} ({product.Category}) - {product.Price} $");
+            }
+
         }
     }
 }
