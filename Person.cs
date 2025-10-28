87d8e19358f93318bfec65d7fa7cce9ccaaddf91
Gabrovez <gabrovez321@gmail.com>
2025-10-28 23:44:42 +0300

    Шаг 2. Наследование

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katarzhin_ISP_232_Lab_10
+{
+    internal class Person
+    {
+        public string? Name { get; set; }
+        public void SayHello()
+        {
+            Console.WriteLine($"Hi, my name is {Name}!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,28 +4,12 @@
     {
         static void Main(string[] args)
         {
-            var monster = new
-            {
-                Name = "Dragon",
-                Damage= 50,
-                IsBoss = true,
-                Description = "Ancinet fire-breathong serpent"
+            Student student = new Student();
+            student.Name = "Grisha";
+            student.University = "VF VolGU";
+            student.SayHello();
+            Console.WriteLine($"Studying in: {student.University}");
 
-            };
-
-            Console.WriteLine(monster.Description);
-            var products = new[]
-            {
-                new {Name = "Milk", Price = 59.99, Category = "Products"},
-                new {Name = "Laptop", Price = 49990.00, Category = "Electronics"},
-                new {Name = "Teethbrush", Price = 120.50, Category = "Hygiene"}
-            };
-
-            Console.WriteLine("List of products");
-            foreach(var product in products)
-            {
-                Console.WriteLine($"{product.Name} ({product.Category}) - {product.Price} $");
-            }
 
         }
     }
