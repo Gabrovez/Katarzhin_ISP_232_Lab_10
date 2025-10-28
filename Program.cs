bc1887c6e30d0a4832650ce0bbcf0a052e11e909
Gabrovez <gabrovez321@gmail.com>
2025-10-29 02:55:18 +0300

    Задание 1 — Анонимные типы

--- Program.cs
+++ Program.cs
@@ -7,27 +7,36 @@ namespace Katarzhin_ISP_232_Lab_10
     {
         static void Main(string[] args)
         {
-            Arena arena = new();
-            arena.AddUnit(new Warrior("Ragnar"));
-            arena.AddUnit(new Mage("Gendalf"));
-            arena.AddUnit(new Archer("Legolas"));
-            arena.StartBattle();
+            var student = new {Name = "Grisha", Age = 18, Group = "ISP-232" };
+            Console.WriteLine($"Name: {student.Name}, Age: {student.Age}, Group: {student.Group}");
+
+            var students = new[]
+            {
+                 new {Name = "Grisha", Age = 19, Group = "ISP-232" },
+                 new { Name = "Anton", Age = 20, Group = "ISP-202" },
+                 new {Name = "Kirill", Age = 22, Group = "ESP-232" },
+                 new {Name = "Oleg", Age = 15, Group = "ISP-282" },
+                 new {Name = "Gosha", Age = 22, Group = "USP-222" },
+            };
+            Console.WriteLine("Student's Table");
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("| {0, -10} | {1, -10} | {2, -10} | ", "Name", "Age", "Group");
+            Console.WriteLine(new string('-', 40));
+            foreach( var s in students)
+            {
+                Console.WriteLine("| {0, -10} | {1, -10} | {2, -10} | ", s.Name, s.Age, s.Group);
+            }
+            Console.WriteLine(new string('-', 40));
+            // На случай если вас будет смущать как я сделал таблицу, то передавать так переменные и у вас на предмете было, а на расте мы так постоянно пишем 
+            // Чёрточки так ставил ещё на питоне когда шахматы делал там знак между ними ставили, и добавил здесь скобочки и вышло что они работают
+
+
+
+
         }
-        
 
-        static IEnumerable<int> GenerateNumbers()
-        {
-            Console.WriteLine("Starting generation...");
-            yield return 1;
 
-            Console.WriteLine("Pause...");
-            yield return 2;
 
-            Console.WriteLine("And one more time...");
-            yield return 3;
-
-            Console.WriteLine("Done!");
-        }
         //static void Main(string[] args)
         //static async Task Main()
     }
