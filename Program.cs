ec66880d351dfe5a75ca9b742cd9bee77e342972
Gabrovez <gabrovez321@gmail.com>
2025-10-29 00:04:29 +0300

    Шаг 6. Лямбды

--- Program.cs
+++ Program.cs
@@ -4,15 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Dog dog = new Dog { Name = "Bobi" };
-            Cat cat = new Cat { Name = "Smokey"};
-            dog.Info();
-            dog.MakeSound();
-            cat.Info();
-            cat.MakeSound();
+            int Square(int x)
+            {
+                return x * x;
+            }
+            Console.WriteLine(Square(5));
 
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(5));
+            Button button = new Button();
+            button.OnClick += () => Console.WriteLine("Buttom pressed!");
+            button.Click();
 
-
+        }
+    }
+    public class Button
+    {
+        public event Action OnClick;
+        public void Click()
+        {
+            OnClick?.Invoke();
         }
     }
 }
