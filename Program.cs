2e0f1b6f87cc321e66714f31713e9b5c880354e9
Gabrovez <gabrovez321@gmail.com>
2025-10-29 03:54:17 +0300

    Задание 5 — Асинхронное программирование

--- Program.cs
+++ Program.cs
@@ -5,18 +5,21 @@ namespace Katarzhin_ISP_232_Lab_10
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            List<string> cities = new List<string> {"Moscow", "Volzhsky", "Sochi", "Anapa", "Kazan'", "Kaliningrad", "Krasnodar"};
-            var citiesm = cities
-                .Where(city => city.StartsWith("K") && city.Length > 6)
-                .OrderBy(city => city.Length);
-            foreach( var c in citiesm)
-            {
-                Console.WriteLine(c);
-            }
+            Console.WriteLine("Wainting for data...");
+            int datar = await GetDataAsync();
+            Console.WriteLine($"Result: {datar}");
+            Console.WriteLine("Data ready!");
+            
 
 
+
+        }
+        static async Task<int> GetDataAsync()
+        {
+            await Task.Delay(2000);
+            return 11;
         }
 
 
