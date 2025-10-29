c1a20c6129d6c2e113c9e73cdde7b9967c1319c2
Gabrovez <gabrovez321@gmail.com>
2025-10-29 03:03:45 +0300

    Задание 2 — Лямбда-выражения

--- Program.cs
+++ Program.cs
@@ -7,28 +7,14 @@ namespace Katarzhin_ISP_232_Lab_10
     {
         static void Main(string[] args)
         {
-            var student = new {Name = "Grisha", Age = 18, Group = "ISP-232" };
-            Console.WriteLine($"Name: {student.Name}, Age: {student.Age}, Group: {student.Group}");
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            Console.WriteLine("Numbers: " + string.Join(", ", numbers));
 
-            var students = new[]
-            {
-                 new {Name = "Grisha", Age = 19, Group = "ISP-232" },
-                 new { Name = "Anton", Age = 20, Group = "ISP-202" },
-                 new {Name = "Kirill", Age = 22, Group = "ESP-232" },
-                 new {Name = "Oleg", Age = 15, Group = "ISP-282" },
-                 new {Name = "Gosha", Age = 22, Group = "USP-222" },
-            };
-            Console.WriteLine("Student's Table");
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine("| {0, -10} | {1, -10} | {2, -10} | ", "Name", "Age", "Group");
-            Console.WriteLine(new string('-', 40));
-            foreach( var s in students)
-            {
-                Console.WriteLine("| {0, -10} | {1, -10} | {2, -10} | ", s.Name, s.Age, s.Group);
-            }
-            Console.WriteLine(new string('-', 40));
-            // На случай если вас будет смущать как я сделал таблицу, то передавать так переменные и у вас на предмете было, а на расте мы так постоянно пишем 
-            // Чёрточки так ставил ещё на питоне когда шахматы делал там знак между ними ставили, и добавил здесь скобочки и вышло что они работают
+            List<int> evennumbers = numbers.FindAll(x => x % 2 == 0);
+            Console.WriteLine("Even numbers: " + string.Join(", ", evennumbers));
+
+            List<int> threevennumbers = numbers.FindAll(x => x % 3 == 0);
+            Console.WriteLine("Multiples of 3: " + string.Join(", ", threevennumbers));
 
 
 
