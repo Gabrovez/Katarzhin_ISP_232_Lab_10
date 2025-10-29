221655cbfc19735018fcf4d85b2ebbaea43f27ac
Gabrovez <gabrovez321@gmail.com>
2025-10-29 03:46:46 +0300

    Задание 4 — LINQ-запросы

--- Program.cs
+++ Program.cs
@@ -7,19 +7,14 @@ namespace Katarzhin_ISP_232_Lab_10
     {
         static void Main(string[] args)
         {
-            List<IPrintable> items = new List<IPrintable>
+            List<string> cities = new List<string> {"Moscow", "Volzhsky", "Sochi", "Anapa", "Kazan'", "Kaliningrad", "Krasnodar"};
+            var citiesm = cities
+                .Where(city => city.StartsWith("K") && city.Length > 6)
+                .OrderBy(city => city.Length);
+            foreach( var c in citiesm)
             {
-                new Bookdz("War and peace", "Lev Tolstoi"),
-                new Bookdz("Stainless steel rat", "Harry Harrison"),
-                new Magazine("Forbes", 27),
-                new Magazine("National Geographic", 11),
-            };
-
-            foreach (var it in items)
-            {
-                it.PrintInfo();
+                Console.WriteLine(c);
             }
-
 
 
         }
