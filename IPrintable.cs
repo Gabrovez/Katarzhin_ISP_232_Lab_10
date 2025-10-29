59d8fb74d154c6c664ef2bb0e8f358b5fb4a30aa
Gabrovez <gabrovez321@gmail.com>
2025-10-29 03:36:59 +0300

    Задание 3 — Интерфейсы

---
+++ IPrintable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katarzhin_ISP_232_Lab_10
+{
+    public interface IPrintable
+    {
+        public void PrintInfo();
+    }
+    public class Bookdz : IPrintable
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Bookdz(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Book: {Title}, Author: {Author}");
+        }
+    }
+
+    public class Magazine : IPrintable
+    {
+        public string Title { get; set; }
+        public int Issue { get; set; }
+
+        public Magazine(string title, int issue)
+        {
+            Title = title;
+            Issue = issue;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Magazine: {Title}, Issue: {Issue}");
+        }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -7,15 +7,18 @@ namespace Katarzhin_ISP_232_Lab_10
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            Console.WriteLine("Numbers: " + string.Join(", ", numbers));
+            List<IPrintable> items = new List<IPrintable>
+            {
+                new Bookdz("War and peace", "Lev Tolstoi"),
+                new Bookdz("Stainless steel rat", "Harry Harrison"),
+                new Magazine("Forbes", 27),
+                new Magazine("National Geographic", 11),
+            };
 
-            List<int> evennumbers = numbers.FindAll(x => x % 2 == 0);
-            Console.WriteLine("Even numbers: " + string.Join(", ", evennumbers));
-
-            List<int> threevennumbers = numbers.FindAll(x => x % 3 == 0);
-            Console.WriteLine("Multiples of 3: " + string.Join(", ", threevennumbers));
-
+            foreach (var it in items)
+            {
+                it.PrintInfo();
+            }
 
 
 
