f8397557b4f548b2307825c9085a1c09f04e8be3
Gabrovez <gabrovez321@gmail.com>
2025-10-29 04:47:49 +0300

    Задание 6 — Мини-проект "Университет"

---
+++ IPerson.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katarzhin_ISP_232_Lab_10
+{
+    internal interface IPerson
+    {
+        public void GetDescription();
+    }
+    public class Studentdz : IPerson
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Group { get; set; }
+
+        public Studentdz(string name, int age, string group)
+        {
+            Name = name;
+            Age = age;
+            Group = group;
+        }
+
+        public void GetDescription()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Group: {Group}");
+        }
+
+    }
+    public class Teacher : IPerson
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Department { get; set; }
+
+        public Teacher(string name, int age, string department)
+        {
+            Name = name;
+            Age = age;
+            Department = department;
+        }
+
+        public void GetDescription()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Department: {Department}");
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,52 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Cache;
 
 namespace Katarzhin_ISP_232_Lab_10
 {
     internal class Program
     {
+        public static async Task LoadStudentsAsync()
+        {
+            Console.WriteLine("Data is loading....");
+            await Task.Delay(3000);
+            Console.WriteLine("Data is ready");
+        }
         static async Task Main()
         {
-            Console.WriteLine("Wainting for data...");
-            int datar = await GetDataAsync();
-            Console.WriteLine($"Result: {datar}");
-            Console.WriteLine("Data ready!");
-            
+            List<Studentdz> students = new()
+            {
+                new Studentdz ("Andrei", 18, "ISP-231"),
+                new Studentdz ("Vika", 19, "ISP-233"),
+                new Studentdz ("Denis", 20, "USP-232"),
+                new Studentdz ("Lena", 21, "USP-232" ),
+                new Studentdz ("Grisha", 22, "ISP-232" )
+            };
+
+            List<Teacher> teachers = new()
+            {
+                new Teacher ("Anton", 28, "ISP"),
+                new Teacher ("Lera", 25, "ISP"),
+                new Teacher ("Oleg", 23, "USP"),
+            };
+            await LoadStudentsAsync();
+            Console.WriteLine();    
+
+            var studover20 = students.Where(s => s.Age > 20).ToList();
+            studover20.Sort((s1,s2) => string.Compare(s1.Name, s2.Name));
+            foreach (var s in studover20)
+            {
+                s.GetDescription();
+            }
+            Console.WriteLine();
+            var teacherop = teachers.Where(s => s.Department == "ISP").ToList();
+            teacherop.Sort((s1, s2) => string.Compare(s1.Name, s2.Name));//Я только щас подумал что можно было order by, но он и так правильно выводит так переписывать не стану уже
+            foreach (var t in teacherop)
+            {
+                t.GetDescription();
+            }
 
 
-
-        }
-        static async Task<int> GetDataAsync()
-        {
-            await Task.Delay(2000);
-            return 11;
         }
 
 
